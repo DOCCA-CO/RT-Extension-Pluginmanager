aadcad0c1ce9a1f6c2abd3d45ddfb1cc2909fbe0
PPetky <peter.petky@gmail.com>
2019-08-19 15:08:42 +0200

    19-08-2019 UI improvements with DataTables

--- lib/RT/Extension/Pluginmanager.pm
+++ lib/RT/Extension/Pluginmanager.pm
@@ -1,5 +1,8 @@
 package RT::Extension::Pluginmanager;
 
-our $VERSION = '0.2.19 b0002';
+our $VERSION = '0.2.19 b1508';
+
+RT->AddStyleSheets('jquery.dataTables.min.css');
+RT->AddStyleSheets('responsive.dataTables.min.css');
 
 1;
